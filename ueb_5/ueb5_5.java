5068785884083a286342117d0bcb12b96757a052
Christian Roth <christian.r.dev@googlemail.com>
2015-12-02 14:05:31 +0100

    Rot13 mit File Ein-/Ausgabe

--- ueb_5/ueb5_5.java
+++ ueb_5/ueb5_5.java
@@ -6,11 +6,11 @@ public class ueb5_5 {
 	public static void main(String [] args) throws Exception {
 		String string;
 		Scanner input = new Scanner(System.in);
-		File file = new File("geheimnis.txt");
-		Scanner fileinput = new Scanner(new FileInputStream(file));
-		PrintWriter writer = new PrintWriter(new FileOutputStream(file));
+		
+		Scanner fileinput = new Scanner(new File("geheimnis.txt"));
 
-		string = fileinput.next();
+		string = fileinput.next();  
+
 		char arr[] = new char[string.length()];
 
 		int index = 0;
@@ -21,6 +21,11 @@ public class ueb5_5 {
 			}
 		}
 
+		fileinput.close(); // scanner schließen bevor wir weiter machen sonst gibt das eventuell probleme
+
+	 	// nicht ein file gleichzeitig öffnen zum schreiben und lesen das gibt probleme	
+		PrintWriter writer = new PrintWriter(new FileOutputStream(new File("geheimnis.txt")));
+
 		for (int i = 0; i < index; i++) {
 			if (arr[i] > 'Z') { // großbuchstaben kommen bei ascii zuerst 
 				arr[i] = (char) ((((arr[i] + 13) - 'a') % 26) + 'a');
@@ -30,7 +35,8 @@ public class ueb5_5 {
 		}
 		writer.println(new String(arr));
 		System.out.println(new String(arr));
-		writer.flush();
-		writer.close();
+
+		writer.flush(); // schreibe alle änderungen bevor wir schließen
+		writer.close(); // schließen und fertig
 	}
 }
