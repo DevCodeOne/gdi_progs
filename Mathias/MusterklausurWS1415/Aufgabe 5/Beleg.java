df040b461d37dc40b4d832fca5bad6499c99ebaa
Mathias83 <mathias_fedder@gmx.net>
2016-01-04 14:54:50 +0100

    Geändert
    Variablen auf Private gesetzt

--- Mathias/MusterklausurWS1415/Aufgabe 5/Beleg.java
+++ Mathias/MusterklausurWS1415/Aufgabe 5/Beleg.java
@@ -1,9 +1,9 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
 class Beleg{
-	static int lfdnr;
-	static int date;
-	static SimpleDateFormat format3 = new SimpleDateFormat("ddMMyy");
+	private static int lfdnr;
+	private static int date;
+	private static SimpleDateFormat format3 = new SimpleDateFormat("ddMMyy");
 	int BelegNr;
 	Beleg() {
 		if (date != Integer.parseInt(format3.format(new Date()))) lfdnr = 0;
