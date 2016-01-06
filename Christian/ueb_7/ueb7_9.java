cfad360f64a149a96b77e1f98fb0943f0037b404
DevCodeOne <christian.r.dev@googlemail.com>
2016-01-06 10:56:46 +0100

    Kleine Korrektur

--- Christian/ueb_7/ueb7_9.java
+++ Christian/ueb_7/ueb7_9.java
@@ -1,15 +1,17 @@
 public class ueb7_9 {
 
 	public static void main(String [] args) {
-
+		try {
+			sichere_division(10, 0);
+		} catch (ArithmeticException e) {	
+			System.err.println(e.getMessage());
+		}
 	}
 
 	public static int sichere_division(int dividend, int teiler) throws ArithmeticException {
-		try {
-			return dividend / teiler;
-		} catch (ArithmeticException e) {
-			System.err.println(e.getMessage());
-		}
-		return -1;
+		if (teiler == 0) 
+			throw new ArithmeticException("Nicht durch null");
+		
+		return dividend / teiler;
 	}
 }
